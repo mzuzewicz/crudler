796b8bf0d4f3066170232b6d561f5e915c09c7f4
mzuzewicz <max.zuzewicz@gmail.com>
2024-10-02 17:28:41 +0100

    Populating Module List (#2)
    Co-authored-by: Max Zuzewicz <K2355696@kingston.ac.uk>

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,18 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, Pressable } from 'react-native';
 import Screen from '../layout/Screen';
-
+import initialModules from '../../data/modules.js';
+ 
 const ModuleListScreen = () =>{
   //Initialisations
+  const modules = initialModules;
   //State
   //Handlers
+  const handleSelect = () => alert("Item selected");
   //View
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        {modules.map((module) => {
+            return (
+              <Pressable key={module.ModuleCode} onPress={handleSelect}>
+                <View style={styles.item}>
+                  <Text style={styles.text}>
+                    {module.ModuleCode} {module.ModuleName}
+                  </Text>
+                </View>
+              </Pressable>
+            );
+          })}
+      </ScrollView>
     </Screen>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+
+  },
+
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgray',
+  },
+
+  text: {
+    fontSize: 16,
+  },
+});
 
 export default ModuleListScreen;
