9f41d44f4a914a171c7edb34d6aa1db0f27ec376
Max Zuzewicz <K2355696@kingston.ac.uk>
2024-10-08 17:23:56 +0100

    Extracting reusable components from ModuleListScreen

---
+++ src/components/entity/modules/ModuleList.js
@@ -0,0 +1,29 @@
+import { StyleSheet, ScrollView } from "react-native";
+import ModuleItem from "./ModuleItem.js";
+
+const ModuleList = ({ modules, onSelect }) => {
+	//Initialisations
+
+	//State
+
+	//Handlers
+
+	//View
+	return (
+		<ScrollView style={styles.container}>
+			{modules.map((module) => {
+				return (
+					<ModuleItem
+						key={module.ModuleCode}
+						module={module}
+						onSelect={onSelect}
+					/>
+				);
+			})}
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleList;
--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,47 +1,27 @@
-import { StyleSheet, ScrollView, Text, View, Pressable } from 'react-native';
-import Screen from '../layout/Screen';
-import initialModules from '../../data/modules.js';
- 
-const ModuleListScreen = () =>{
-  //Initialisations
-  const modules = initialModules;
-  //State
-  //Handlers
-  const handleSelect = () => alert("Item selected");
-  //View
-  return (
-    <Screen>
-      <ScrollView style={styles.container}>
-        {modules.map((module) => {
-            return (
-              <Pressable key={module.ModuleCode} onPress={handleSelect}>
-                <View style={styles.item}>
-                  <Text style={styles.text}>
-                    {module.ModuleCode} {module.ModuleName}
-                  </Text>
-                </View>
-              </Pressable>
-            );
-          })}
-      </ScrollView>
-    </Screen>
-  );
+import { StyleSheet } from "react-native";
+import Screen from "../layout/Screen";
+import initialModules from "../../data/modules.js";
+import ModuleList from "../entity/modules/ModuleList.js";
+
+const ModuleListScreen = () => {
+	//Initialisations
+	const modules = initialModules;
+
+	//State
+
+	//Handlers
+	const handleSelect = (module) => alert(`Item ${module.ModuleCode} selected`);
+
+	//View
+	return (
+		<Screen>
+			<ModuleList modules={modules} onSelect={handleSelect} />
+		</Screen>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
-
-  item: {
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-
-  text: {
-    fontSize: 16,
-  },
+	container: {},
 });
 
 export default ModuleListScreen;
